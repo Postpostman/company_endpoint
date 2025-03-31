3d220fda0ace52b2e27b67457cf0d10a8ec5f71f
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-04-01 00:50:47 +0300

    create simple CompanyViewSet

--- company/views.py
+++ company/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from company.models import Company
+from company.serializers import CompanySerializer
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    permission_classes = [IsAuthenticated]
