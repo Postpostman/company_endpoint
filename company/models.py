8d38b92ff51548bf48d4582a964f35812fd0840c
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-04-01 00:44:36 +0300

    create first Company model in main app with 8 fields and redefined __str__ method

--- company/models.py
+++ company/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Company(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=255, unique=True)
+    company_address = models.TextField(blank=True, null=True)
+    company_email = models.EmailField(unique=True)
+    company_phone = models.CharField(max_length=20, blank=True, null=True)
+    user1 = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='company_user1', blank=True, null=True)
+    user2 = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='company_user2', blank=True, null=True)
+    user3 = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='company_user3', blank=True, null=True)
+
+    def __str__(self):
+        return self.company_name
