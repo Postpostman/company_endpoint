cba09c364aefd911aa61a6c8855777b1f98cc647
Roman <roman.vynnytskyi.tt@gmail.com>
2025-04-01 22:37:41 +0300

    add related names to User model because of error

--- user/models.py
+++ user/models.py
@@ -6,5 +6,20 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(max_length=50, unique=True)
 
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions'
+    )
+
     def __str__(self):
         return self.email
