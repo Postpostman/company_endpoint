3b2c8fd2e26c0fe0d49accc81df70cae6ab5fccc
Roman <roman.vynnytskyi.tt@gmail.com>
2025-04-01 21:59:27 +0300

    create User model with email field by AbstarctUser

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.email
